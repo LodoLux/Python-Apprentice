a7695157f0445812cd2fb50c3f1ebdd53dd5c8e9
LodoLux <gamevisiontv9@gmail.com>
2025-04-16 14:57:38 -0700

    Made two squares with lists

--- lessons/00_Turtles/12_Color_Lines.py
+++ lessons/00_Turtles/12_Color_Lines.py
@@ -6,7 +6,7 @@ Color Lines
 """
 
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+turtle.setup (width=640, height=360)# Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
@@ -14,14 +14,30 @@ tina.shape('turtle')                    # Set the shape of the turtle to a turtl
 tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
 
 
-colors = [ 'red', 'blue', 'black', 'orange']    # define a list of colors
+colors = [ 'red', 'blue', 'pink', 'orange']    # define a list of colors
+tina.pensize(15)
 
 for color in colors:                            # loop through the colors
-    ... # Your code here
+    tina.pencolor(color)
+    tina.forward(50)
+    tina.left(90)
+
+tina.penup()
+tina.left(180)
+tina.forward(100)
+tina.pendown()
+
+# 2) Make another square, but put the colors in reverse order, using a negative index.
+length = len(colors)
+for i in range(length):
+    tina.pencolor(colors[length - 1 - i])
+    tina.forward(100)
+    tina.left(90) #set to 90 later
 
 
-# 2) Make another square, but put the colors in reverse order, using a negative index. 
 
-... # Your code here
+
+
+
 
 turtle.exitonclick()                     # Close the window when we click on it
